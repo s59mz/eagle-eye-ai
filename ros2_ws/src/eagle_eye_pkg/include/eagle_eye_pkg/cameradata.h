c93f7d34bdab2c95d285c18bf1293a107f9cf1db
Matjaz <test@example.com>
2024-07-25 21:44:32 +0200

    added file header

--- ros2_ws/src/eagle_eye_pkg/include/eagle_eye_pkg/cameradata.h
+++ ros2_ws/src/eagle_eye_pkg/include/eagle_eye_pkg/cameradata.h
@@ -1,5 +1,24 @@
+/*
+# Eagle-Eye-AI
+# Smart Following Camera with Face Recognition
+#   for Kria KR260 Board
+#
+# Created by: Matjaz Zibert S59MZ - July 2024
+#
+# GStreamer Pipeline
+# Camera Orientation custom user data 
+#   - Put this struct in the reserved_1 pointer field
+#   - The GStreamer probe populates Inference Metadata with this struct
+#     with the data read from the camera Inclinometer, so the VVAS Draw
+#     Filter can show the Camera's Azimuth and Elevation Status
+#     in the video stream.
+#
+# Design based on Kria KV260 Smartcam Demo App by AMD
+#
+# Hackster.io Project link:
+#     https://www.hackster.io/matjaz4
+*/
 
-// Camera Orientation custom user data - put in the reserved_1 field
 typedef struct _CameraOrientation
 {
   float azimuth;
